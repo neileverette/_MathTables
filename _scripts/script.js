bfc184fc323f7f559a99054cc69deb355bd53765
Neil Everette <neil.everette@gmail.com>
2013-06-03 04:56:39 -0700

    Working selectable dropdown menus
    The two drop down menus are data binded together

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,6 +1,17 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	console.log('this is working');
-	alert('I work');
-	})
+	
+	$('#number1').change(function(){
+		
+		// LOGIC TO MAKE SURE SECOND NUMBER DOESN'T EXCEED 20
+		var var_number = parseInt($('#number1').val())
+		if(var_number < 20){
+			var_number = var_number+1;
+			}
+		
+		// THIS CHANGES THE SECOND SELECTOR BASED ON THE FIRST SELECTOR
+		$('#number2').replaceWith(
+			'<select id="number2"><option value = '+ var_number +'>' + var_number + '</option></select>');
+	});
+})
